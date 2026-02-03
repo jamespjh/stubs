adeb73a3dd81b00bea31f51e232c63090847c3b8
James Hetherington <j.hetherington@ucl.ac.uk>
2026-02-03 11:13:20 +0000

    Fix url for jekyll test

--- deploy_tests/test_web.py
+++ deploy_tests/test_web.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 def test_fetch_and_content():
-    fetch = requests.get('https://research.jh.condenser.arc.ucl.ac.uk/stubs')
+    fetch = requests.get('https://research.jh.condenser.arc.ucl.ac.uk/stubs/jekyll/')
     model = BeautifulSoup(fetch.text, 'html.parser')
     title = model.find_all("h2",id="jekyll")
     assert len(title) == 1
